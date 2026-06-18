e3980b33789691b285a0f4417f774bd5ff87a47d
Niraj2005kumar <nirajverma075@gmail.com>
2026-06-19 00:01:28 +0530

    dic

--- Dictionary12.py
+++ Dictionary12.py
@@ -16,8 +16,20 @@ d = {10:100,20:200,30:300,40:400}
 # d.clear()
 # print(d)
 
-q = d.fromkeys([10,20,30,40],50)
-print(q)
+# q = d.fromkeys([10,20,30,40],50)  # esme sare KEY KI VALUE ME EK HI VALUE HO JATI HIA YAHI KI 50 DIYA HUYA HAI SAB KEY KA VALUE 50 HO JAYEGA  
+
+# print(d.get(10)) #you can directly print the item   matlab ki key ki value ko direct print kr sakte ho 
+# print(d.items()) # esme ek list bane ga or ek tupple ke formate me dega output example :- dict_items([(10, 100), (20, 200), (30, 300), (40, 400)])
+# print(d.keys())  #esme ki ki value print hoti hai or us key ki output dic ke type me aayega 
+# print(d.values())  #esse key ki value print hoti hai 
+# print(d.pop(20))   #esme jo pop ke under key likhte hai o pop ho jata hai or us key ki value bhi bahar nikl jati hai or jab hum return dic ko print krte hai to 
+                    # key ki value nhi rhti hia example (200
+# d.popitem() # esme directly dic ke last element wali key or value ko pop kr deti hai 
+print(d.setdefault())                                                        # {10: 100, 30: 300, 40: 400})
+print(d)
+
+
+
 
 
 
